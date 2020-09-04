b925297e8926c0ac0d6a52c01fafd2d5ef5e6873
Vincent <lara.vincent@gmail.com>
2020-09-04 18:31:34 +0200

    Use json in region Sud

--- indice_pollution/regions/Sud.py
+++ indice_pollution/regions/Sud.py
@@ -1,6 +1,5 @@
 from . import ForecastMixin
 from datetime import timedelta, datetime
-import xml.etree.ElementTree as ET
 
 class Forecast(ForecastMixin):
     website = 'https://www.atmosud.org/'
@@ -8,6 +7,9 @@ class Forecast(ForecastMixin):
     fr_date_format = '%d-%m-%Y 00:00:00'
 
     insee_list = ['06029', '06088', '13001', '13055', '83137', '84007']
+
+    attributes_key = 'properties'
+    use_dateutil_parser = True
 
     @classmethod
     def params(cls, date_, insee):
@@ -22,29 +24,6 @@ class Forecast(ForecastMixin):
             'version': '1.1.0',
             'request': 'GetFeature',
             'typeName': 'ind_sudpaca:ind_sudpaca_agglo',
-            'CQL_FILTER': f"code_zone='{insee}' AND (date_ech='{fr_date}' OR date_ech='{fr_tomorrow}')"
-        }
-
-    @classmethod
-    def features(cls, r):
-        root = ET.fromstring(r.text)
-        return list(filter(lambda el: el.tag == '{http://ind_sudpaca}ind_sudpaca_agglo', root[0]))
-
-
-    @classmethod
-    def getter(cls, feature):
-        feature_dict = {f.tag: f.text for f in feature}
-
-        return {
-            **{
-            'indice': feature_dict['{http://ind_sudpaca}valeur'],
-            'date': datetime.strptime(
-                    feature_dict['{http://ind_sudpaca}date_ech'], cls.fr_date_format
-                ).strftime('%Y-%m-%d')
-            },
-            **{
-                k: feature_dict[f'{{http://ind_sudpaca}}{k}']
-                for k in cls.outfields
-                if f'{{http://ind_sudpaca}}{k}' in feature_dict
-            }
+            'CQL_FILTER': f"code_zone='{insee}' AND (date_ech='{fr_date}' OR date_ech='{fr_tomorrow}')",
+            'outputFormat': 'json'
         }
