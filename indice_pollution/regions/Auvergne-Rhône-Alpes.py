6965abe44610dded4cec4e05c934dd91696735c4
Vincent <lara.vincent@gmail.com>
2020-09-30 14:27:29 +0200

    Fix Auvergne Rhône Alpes

--- indice_pollution/regions/Auvergne-Rhône-Alpes.py
+++ indice_pollution/regions/Auvergne-Rhône-Alpes.py
@@ -1,4 +1,7 @@
 from . import ForecastMixin
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 class Forecast(ForecastMixin):
     website = 'https://www.atmo-auvergnerhonealpes.fr/'
@@ -9,3 +12,36 @@ class Forecast(ForecastMixin):
         '63300', '01053', '74056', '03310', '03185', '42187', '15014', '38185', '74010',
         '42218', '26362', '38053', '01328', '63113'
     ]
+
+    COLOR_TO_QUALIF = {
+        "0": "tres_bon",
+        "21": "bon",
+        "31": "bon",
+        "41": "bon",
+        "51": "moyen",
+        "61": "mediocre",
+        "71": "mediocre",
+        "81": "mediocre",
+        "91": "mauvais",
+        "100": "tres_mauvais"
+    }
+
+    def get_no_cache(self, date_, insee, attempts=0):
+        r = requests.get(f'https://www.atmo-auvergnerhonealpes.fr/monair/commune/{insee}')
+        r.raise_for_status()
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        controls = soup.find_all('div', class_='day-controls')
+        days = controls[0].find_all('a', class_='raster-control-link')
+
+        return [
+            {
+                "date": str(datetime.fromtimestamp(int(day.attrs.get('data-rasterid'))).date()),
+                "indice": int(int(day.attrs.get('data-index'))/10),
+                "qualif": self.COLOR_TO_QUALIF[day.attrs.get('data-color')]
+            }
+            for day in days
+        ]
+
+    def get_close_insee(self, insee):
+        return insee
