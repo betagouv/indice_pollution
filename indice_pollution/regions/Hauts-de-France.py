c295bf7ac24ae4dc6659b5e49253947430f8877e
Vincent <lara.vincent@gmail.com>
2021-01-20 10:02:39 +0100

    Fix Hauts de France

--- indice_pollution/regions/Hauts-de-France.py
+++ indice_pollution/regions/Hauts-de-France.py
@@ -5,13 +5,10 @@ from dateutil.parser import parse
 class Service(ServiceMixin):
     date_format = '%Y-%m-%d'
     website = 'https://www.atmo-hdf.fr/'
-    insee_list = [
-        '59350', '59183', '59178', '62041', '62160', '59606', '02691', '60175',
-        '62765', '62193', '59392', '80021', '62119'
-    ]
 
 class Forecast(Service, ForecastMixin):
-    url = 'https://services8.arcgis.com/rxZzohbySMKHTNcy/arcgis/rest/services/ind_hdf_agglo/FeatureServer/0/query'
+    url = 'https://services8.arcgis.com/rxZzohbySMKHTNcy/arcgis/rest/services/ind_hdf_2021/FeatureServer/0/query'
+    outfields = '*'
 
 class Episode(Service, EpisodeMixin):
     url = 'https://services8.arcgis.com/rxZzohbySMKHTNcy/arcgis/rest/services/alrt3j_hdf/FeatureServer/0/query'
