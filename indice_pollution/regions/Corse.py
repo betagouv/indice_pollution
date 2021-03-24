d46e2377d1e11d63af576978b4fe21c10db4cde7
Vincent <lara.vincent@gmail.com>
2021-03-24 10:53:16 +0100

    Fix Corse

--- indice_pollution/regions/Corse.py
+++ indice_pollution/regions/Corse.py
@@ -3,17 +3,19 @@ from . import ServiceMixin, ForecastMixin, EpisodeMixin
 class Service(ServiceMixin):
     website =  'https://www.qualitaircorse.org/'
     nom_aasqa = 'Qualitair Corse'
-    insee_list = ['2B096', '2B033', '2A004']
+    #insee_list = ['2B033', '2A004', '2B096']
 
 class Forecast(Service, ForecastMixin):
     url = 'https://services9.arcgis.com/VQopoXNvUqHYZHjY/arcgis/rest/services/indice_atmo_communal_corse/FeatureServer/0/query'
 
     def params(self, date_, insee):
         return {
-            "outFields": "date_ech,code_qual,lib_qual,coul_qual,code_zone,lib_zone,code_no2,code_so2,code_o3,code_pm10,code_pm25",
+            "outFields": "*",
             "outSR": "4326",
             "f":"json",
-            "where": f"code_zone='{insee}' AND date_ech>='{date_}'"
+            "where": f"code_zone='{insee}'",
+            'orderByFields': 'date_ech DESC',
+            'resultRecordCount': 3
         }
 
 class Episode(Service, EpisodeMixin):
