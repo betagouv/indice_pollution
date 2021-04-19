b8e0095a435faf926dca363cc11e409dff90edc1
Vincent <lara.vincent@gmail.com>
2021-04-19 18:17:18 +0200

    Fix indice Nouvelle Aquitaine

--- indice_pollution/regions/Nouvelle-Aquitaine.py
+++ indice_pollution/regions/Nouvelle-Aquitaine.py
@@ -84,7 +84,7 @@ class Forecast(Service, ForecastMixin):
         return [
             self.getter({
                 "date": str(datetime.fromtimestamp(int(day.attrs.get('data-rasterid'))).date()),
-                "indice": int(day.attrs.get('data-index')),
+                "indice": int(day.attrs.get('data-index')) - 1,
             })
             for day in days
         ]
