81362750c47bbb6c3eb7d7b82556396c96fcba22
Vincent Lara <lara.vincent@gmail.com>
2022-02-21 16:49:30 +0100

    Version 0.29.2

--- setup.py
+++ setup.py
@@ -25,9 +25,7 @@ DEPENDENCIES = [
 
 setup(
     name='indice_pollution',
-    
-
-    version='0.29.1',
+    version='0.29.2',
     description='API giving air pollution level in France',
     url='https://github.com/l-vincent-l/indice_pollution',
     download_url='https://github.com/l-vincent-l/indice_pollution/archive/0.1.2.tar.gz',
