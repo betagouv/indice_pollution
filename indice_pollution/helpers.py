7e42ef3e80beb8043fe798a6e357354547a2d0ce
Vincent Lara <lara.vincent@gmail.com>
2022-11-09 12:24:05 +0100

    Ajout de ping dans save_all_indices

--- indice_pollution/helpers.py
+++ indice_pollution/helpers.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 import pytz
+from flask import current_app
+import requests
+
 
 def today():
     zone = pytz.timezone('Europe/Paris')
@@ -18,3 +21,9 @@ def oxford_comma(items):
         return '{} et {}'.format(*items)
     else:
         return '{}, et {}'.format(', '.join(items[:-1]), items[-1])
+
+def ping(uuid):
+    try:
+        requests.get(f"https://hc-ping.com/{uuid}", timeout=10)
+    except requests.RequestException as e:
+        current_app.logger.error(f"Ping to healthchecks.io failed: {e}")
