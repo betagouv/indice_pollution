bde527f66aa8cbe934589afd43d919cc0fc35412
Vincent Lara <lara.vincent@gmail.com>
2022-11-17 10:58:15 +0100

    Remove static path

--- indice_pollution/__init__.py
+++ indice_pollution/__init__.py
@@ -132,7 +132,6 @@ def create_app(test_config=None):
     app = Flask(
         __name__,
         instance_relative_config=True,
-        static_url_path=''
     )
     app.config.from_mapping(
         SECRET_KEY=os.getenv('SECRET_KEY', 'dev'),
