74f301ba12ccee430a8611b8990efc9617a3fafb
Vincent Lara <lara.vincent@gmail.com>
2023-01-30 17:14:00 +0100

    bind params

--- migrations/versions/649b57785efc_ajout_zone_regions_ile_de_france.py
+++ migrations/versions/649b57785efc_ajout_zone_regions_ile_de_france.py
@@ -17,14 +17,13 @@ depends_on = None
 
 
 def upgrade():
-    zone_id = op.get_bind().execute("INSERT INTO indice_schema.zone (code, type) VALUES ('11', 'region') RETURNING id").fetchone()
+    zone_id = op.get_bind().execute(sa.text("INSERT INTO indice_schema.zone (code, type) VALUES ('11', 'region') RETURNING id")).fetchone()
 
     op.get_bind().execute(sa.text("""
         UPDATE indice_schema.commune
         SET zone_pollution_id = :zone
         WHERE departement_id IN (select d.id FROM indice_schema.departement d JOIN indice_schema.region r on d.region_id = r.id WHERE r.code = '11')
-    """), {"zone": zone_id[0]})
-
+    """).bindparams(zone=zone_id[0]))
 
 
 def downgrade():
